0511d116abc3c445a7324c8fbfbbb9b1d483195c
Carsten Sauerbrey <carsten.sauerbrey@gmail.com>
2021-06-12 13:15:54 +0200

    Improving CI

--- update.sh
+++ update.sh
@@ -6,6 +6,10 @@ PYTHON_VERSION=$2
 echo "Downloading Julia Dockerfile for version ${JULIA_VERSION}"
 curl -s -o Dockerfile "https://raw.githubusercontent.com/docker-library/julia/master/${JULIA_VERSION}/buster/Dockerfile"
 
+if [ ! -f "Dockerfile" ]; then
+    exit 1
+fi
+
 echo "Patching Julia Dockerfile"
 sed -i \
 -e "s/FROM debian:buster-slim/FROM python:${PYTHON_VERSION}-buster/g" \
